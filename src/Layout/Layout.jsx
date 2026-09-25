eccf1e18153c0fb1975fc56b8ce019ebd3679c60
KarimAshraf4115 <karim.ashraf.4115@gmail.com>
2026-09-25 18:24:54 +0300

    fix navigation problem (scroll)

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Common/Navbar/Navbar";
 import Footer from "../Components/Common/Footer/Footer";
+import ScrollToTop from './../Components/Common/ScrollToTop/ScrollToTop';
 
 export default function Layout() {
   return (
     <>
+      <ScrollToTop  />
       <Navbar />
       <div className="pt-20 grow">
         <Outlet />
